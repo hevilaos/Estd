e53a384cd00e8a9e95f86122017400136f92a4e3
hevilaos <hevilaos@gmail.com>
2022-10-09 15:03:56 -0300

    Primeira mudança
    Trazendo o projeto de pasta local para repositório local

---
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.js
@@ -0,0 +1,38 @@
+
+describe('Our first suite', () => {
+
+    describe('Our suite section', () => {
+        
+        if('some test name', () =>{
+
+        });
+
+    })
+
+    if('first test', () => {
+
+    });
+
+    if('second test', () => {
+
+    });
+
+    if('third test', () => {
+
+    });
+})
+
+describe('Our second suite', () => {
+
+    if('first test', () => {
+
+    });
+
+    if('second test', () => {
+
+    });
+
+    if('third test', () => {
+
+    });
+})
