68d3ce018077f7b33f0a242b8b42360b1061d6fa
hevilaos <hevilaos@gmail.com>
2022-10-09 15:49:31 -0300

    Update firstTest.spec.js
    Change after course cap 10

--- ngx-cypress-test/cypress/e2e/firstTest.spec.js
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.js
@@ -1,38 +1,9 @@
 
 describe('Our first suite', () => {
 
-    describe('Our suite section', () => {
-        
-        if('some test name', () =>{
-
-        });
-
-    })
-
     if('first test', () => {
 
     });
 
-    if('second test', () => {
-
-    });
-
-    if('third test', () => {
-
-    });
 })
 
-describe('Our second suite', () => {
-
-    if('first test', () => {
-
-    });
-
-    if('second test', () => {
-
-    });
-
-    if('third test', () => {
-
-    });
-})
