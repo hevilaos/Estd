82b2b5dd9d08127ed4ad18618152a83b288cf15c
hevilaos <hevilaos@gmail.com>
2022-11-09 11:51:35 -0300

    Chap 17 changes

--- ngx-cypress-test/cypress/e2e/firstTest.spec.ts
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.ts
@@ -166,7 +166,7 @@ describe('Our first suite', () => {
         cy.get('[type="checkbox"]').eq(1).check({force:true})
     })
 
-    it.only('lists and dropdowns', () => {
+    it('lists and dropdowns', () => {
         cy.visit('http://localhost:4200/')
 
         //1
@@ -196,6 +196,48 @@ describe('Our first suite', () => {
                 }
             })
         })
+    })
+
+    it.only('Web tables', () => {
+        cy.visit('http://localhost:4200/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1
+        cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        //2
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+        cy.get('tbody tr').first().find('td').then( tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Artem')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Bondar')
+        })
+
+        //3
+        const age = [20, 30, 40, 200]
+        cy.wrap(age).each( age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age) 
+            cy.wait(500)
+            cy.get('tbody tr').each( tableRow => {
+                if(age == 200){
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                }else {
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+            })
+        })
+
+
     })
 })
 
